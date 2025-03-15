20a0f1fef4cd7335729d990f0273c43daf9fc913
Blondwolf <vulliemin.k@gmail.com>
2025-03-15 21:16:36 +0100

    Working on camera

--- Assets/CameraManager.cs
+++ Assets/CameraManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    public CinemachineBrain cinemachine;
     public CinemachineVirtualCamera globalCamera;
     
     public List<CinemachineVirtualCamera> camerasInstruments = new List<CinemachineVirtualCamera>();
@@ -11,21 +12,25 @@ public class CameraManager : MonoBehaviour
 
     public void Zoom()
     {
-
+        globalCamera.gameObject.SetActive(false);
     }
 
     public void Back()
     {
-
+        globalCamera.gameObject.SetActive(true);
     }
 
     public void NextInstrument()
     {
+        // tODO limit
 
+        currentIndex++;
+        camerasInstruments[currentIndex].gameObject.SetActive(true);
     }
 
     public void PreviousInstrument()
     {
-
+        camerasInstruments[currentIndex].gameObject.SetActive(false);
+        currentIndex--;
     }
 }
